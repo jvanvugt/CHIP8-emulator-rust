b6cf0af99debe0c3ff71d0a3415011c2c39cf32c
Joris van Vugt <jorisvanvugt@gmail.com>
2021-05-28 21:05:13 +0200

    Read rom file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Expected exactly 2 arguments");
+        return
+    }
+    let rom_file = &args[1];
+    let contents = std::fs::read(rom_file).unwrap();
 }
